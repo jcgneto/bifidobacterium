23f8fd121f5d3da9be01979864c6eb90de55d149
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-10-15 12:38:19 -0500

    Update roary_phylogroup4.sh

--- roary_phylogroup4.sh
+++ roary_phylogroup4.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=benson
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --mem=100GB
+#SBATCH --job-name="roaryphylogroup4"
+#SBATCH --error="roaryphylogroup4.%J.error"
+#SBATCH --output="roaryphylogroup4.%J.out"
+
+#module load
+
+module load python/3.7
+module load roary/3.12
+
+DATAIN=/work/benson/netogomes/bifido/data/trimmed_files/paired/assemblies/contigs/prokkaoutput/roary/phylogroup4
+DATAOUT=/work/benson/netogomes/bifido/data/trimmed_files/paired/assemblies/contigs/prokkaoutput/roary/phylogroup4
+
+cd $DATAIN
+
+
+roary -e --mafft -p 12 -i 95 -cd 100 *.gff -f $DATAOUT
 
