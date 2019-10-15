d5cd241d93176468b5a1e99a00afb8ef6914a6a5
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-10-15 11:51:58 -0500

    Update prokka_genus.sh

--- prokka_genus.sh
+++ prokka_genus.sh
@@ -1,10 +1,10 @@
 # Here is the program to create your own genus database inside Prokka
 
 prokka-genbank_to_fasta_db bifidobacterium1.gbk (add as many .gbk files as you want - download then from NCBI) > Bifidobacterium.faa
-cd-hit -i Coccus.faa -o Coccus -T 0 -M 0 -g 1 -s 0.8 -c 0.9
-rm -fv Coccus.faa Coccus.bak.clstr Coccus.clstr
-makeblastdb -dbtype prot -in Coccus
-mv Coccus.p* /path/to/prokka/db/genus/
+cd-hit -i Bifidobacterium.faa -o Bifidobacterium -T 0 -M 0 -g 1 -s 0.8 -c 0.9
+rm -fv Bifidobacterium.faa Bifidobacterium.bak.clstr Bifidobacterium.clstr
+makeblastdb -dbtype prot -in Bifidobacterium
+mv Bifidobacterium.p* /path/to/prokka/db/genus/
 
 
 More detailed information can be found here:
