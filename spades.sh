b8d24bd3b6eff9d497431f1a63d7dc170273b16d
Joao Carlos Gomes Neto <jgomesneto2@unl.edu>
2019-10-15 12:21:17 -0500

    Create spades.sh

---
+++ spades.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --partition=benson
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=72:00:00
+#SBATCH --mem=100GB
+#SBATCH --job-name="spades"
+#SBATCH --error="spades.%J.error"
+#SBATCH --output="spades.%J.out"
+
+#module load
+
+module load spades/py36/3.13
+
+#enter the directory
+
+DATAIN=/work/benson/netogomes/bifido/data/trimmed_files/paired
+DATAOUT=/work/benson/netogomes/bifido/data/trimmed_files/paired/assemblies
+
+cd $DATAIN
+
+##Assembly of all genomes simultaneously 
+
+for file1 in *R1*fastq
+do 
+file2=${file1/R1/R2}
+out=${file1%%_R01_001.fastq}_output.fasta
+spades.py -t 12 -1 $file1 -2 $file2 --careful --cov-cutoff auto -o $DATAOUT/$out 
+done
+
+
+echo "Job is done!"
